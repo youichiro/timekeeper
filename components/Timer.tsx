fa4cdbbd45d166b2909df40fdc3d8e4836739b6f
youichiro <cinnamon416@gmail.com>
2020-12-26 13:03:34 +0900

    Timerコンポーネントを作成

---
+++ components/Timer.tsx
@@ -0,0 +1,12 @@
+// import React, { useState, useEffect } from 'react'
+import React from 'react'
+
+const Timer: React.FC = () => {
+  return (
+    <>
+      <p>timer</p>
+    </>
+  )
+}
+
+export default Timer
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import CurrentTime from '../components/CurrentTime'
+import Timer from '../components/Timer'
 
 const IndexPage: React.FC = () => (
   <Layout title="Home">
     <h1>Timekeeper</h1>
     <CurrentTime />
+    <Timer />
   </Layout>
 )
 
