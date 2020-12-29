03e78c1aa5b22a68db5a202609d2049620e9f10e
youichiro <cinnamon416@gmail.com>
2020-12-29 16:08:05 +0900

    AgendaListコンポーネントを作成

---
+++ components/AgendaList.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AgendaList: React.FC = () => {
+  return (
+    <h2>AgendaList</h2>
+  )
+}
+
+export default AgendaList
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Container } from '@material-ui/core'
 import Layout from '../components/Layout'
-// import CurrentTime from '../components/CurrentTime'
 import WholeTimer from '../components/WholeTimer'
+import AgendaList from '../components/AgendaList'
 
 const IndexPage: React.FC = () => (
   <Layout title="Home">
     <Container fixed>
       <h1>Timekeeper</h1>
       <WholeTimer />
+      <AgendaList />
     </Container>
   </Layout>
 )
