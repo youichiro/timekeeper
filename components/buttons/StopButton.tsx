9c8eece3ebba758fb4c11217db18c8545947173f
youichiro <cinnamon416@gmail.com>
2020-12-31 23:28:45 +0900

    停止ボタンをいい感じに表示する

--- components/buttons/StopButton.tsx
+++ components/buttons/StopButton.tsx
@@ -2,6 +2,20 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { stopCount } from '../../stores/counter'
 
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
+import PauseIcon from '@material-ui/icons/Pause'
+import Tooltip from '@material-ui/core/Tooltip'
+import Fab from '@material-ui/core/Fab'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    fab: {
+      margin: theme.spacing(2),
+      padding: theme.spacing(5),
+    },
+  })
+)
+
 const StopButton: React.FC = () => {
   const dispatch = useDispatch()
 
@@ -9,11 +23,15 @@ const StopButton: React.FC = () => {
     dispatch(stopCount())
   }
 
+  const classes = useStyles()
+
   return (
     <div>
-      <button type="submit" onClick={onClickStopButton}>
-        停止
-      </button>
+      <Tooltip title="stop">
+        <Fab color="primary" className={classes.fab}>
+          <PauseIcon style={{ fontSize: 50 }} onClick={onClickStopButton} />
+        </Fab>
+      </Tooltip>
     </div>
   )
 }
