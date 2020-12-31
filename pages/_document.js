5161e02bce16eb7f37da8d5dfd0cc338c6af5b10
youichiro <cinnamon416@gmail.com>
2020-12-31 16:06:18 +0900

    material-uiでエラーになるため_document.tsを作成

---
+++ pages/_document.js
@@ -0,0 +1,37 @@
+// https://github.com/mui-org/material-ui/blob/master/examples/nextjs/pages/_document.js
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheets } from '@material-ui/core/styles';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="ja">
+        <Head>
+          <meta charSet="utf-8" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+  };
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,25 @@
 import React from 'react'
+import Head from 'next/head'
 import { Container } from '@material-ui/core'
 import { Provider } from 'react-redux'
 
-import Layout from '../components/Layout'
 import WholeTimer from '../components/WholeTimer'
 import AgendaList from '../components/AgendaList'
 import store from '../stores'
 
 const IndexPage: React.FC = () => (
-  <Provider store={store}>
-    <Layout title="Home">
+  <div>
+    <Head>
+      <title>Timekeeper</title>
+    </Head>
+    <Provider store={store}>
       <Container fixed>
         <h1>Timekeeper</h1>
         <WholeTimer />
         <AgendaList />
       </Container>
-    </Layout>
-  </Provider>
+    </Provider>
+  </div>
 )
 
 export default IndexPage
