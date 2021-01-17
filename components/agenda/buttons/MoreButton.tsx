1ae70b1287978982d531d66ad588cd53c7c90670
youichiro <cinnamon416@gmail.com>
2021-01-17 14:17:08 +0900

    Menuの処理を追加

--- components/agenda/buttons/MoreButton.tsx
+++ components/agenda/buttons/MoreButton.tsx
@@ -1,14 +1,55 @@
-import React from 'react'
-import { IconButton, Tooltip } from '@material-ui/core'
+import React, { useState } from 'react'
+import {
+  IconButton,
+  ListItemIcon,
+  ListItemText,
+  Menu,
+  MenuItem,
+  Tooltip,
+} from '@material-ui/core'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
+import { useDispatch } from 'react-redux'
+import { openTextReaderDialog } from '../../../stores/text-reader-dialog'
+import InsertDriveFileIcon from '@material-ui/icons/InsertDriveFile'
 
 const MoreButton: React.FC = () => {
+  const dispatch = useDispatch()
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+
+  const handleClick = (e: any) => {
+    setAnchorEl(e.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  const handleOpenTextReader = () => {
+    dispatch(openTextReaderDialog())
+    handleClose()
+  }
+
   return (
-    <Tooltip title="more">
-      <IconButton size="small">
-        <MoreHorizIcon />
-      </IconButton>
-    </Tooltip>
+    <div>
+      <Tooltip title="more" onClick={handleClick}>
+        <IconButton size="small">
+          <MoreHorizIcon />
+        </IconButton>
+      </Tooltip>
+      <Menu
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleOpenTextReader}>
+          <ListItemIcon>
+            <InsertDriveFileIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText primary="open text-reader" />
+        </MenuItem>
+      </Menu>
+    </div>
   )
 }
 
